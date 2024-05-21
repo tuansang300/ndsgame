b0b62a2325a687bb5f52345018308cc0f0455bb9
tuansang300 <tuansang314@gmail.com>
2024-05-21 12:43:49 +0700

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
